7069f4695899b612cdafc822b0d9733f411480a8
규찬 조 <qqppqqppqp35@gmail.com>
2023-11-16 17:03:38 +0900

    .

--- PS/PS.cpp
+++ PS/PS.cpp
@@ -1,7 +1,16 @@
 ﻿#include <iostream>
-#include <algorithm>
+#include <vector>
+#include <stack>
 using namespace std;
 
+int N, M, V;
+vector<vector<int>> g[1000][10000];
+
 int main() {
+	cin >> N >> M >> V;
+
+
+
+
 	return 0;
 }
